1d061d5f0c4bda50ce6625384323cc46991ebd1e
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:37 +0200

    Refactored module settings

--- Module.php
+++ Module.php
@@ -43,6 +43,15 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
         return parent::Decorator();
     }
 
+    /**
+     *
+     * @return Settings
+     */
+    protected function GetModuleSettings()
+    {
+        return $this->oModuleSettings;
+    }
+
     /***** private functions *****/
 
     /***** public functions might be called with web API *****/
